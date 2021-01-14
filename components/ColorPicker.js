8bce3a3792fb4c47eb825d62cf0bbb7204613159
Terry Sutton <saltcod@gmail.com>
2021-01-13 21:51:26 -0330

    Remove border

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -45,8 +45,8 @@ export const ColorPicker = () => {
 	}
 
 	return (
-		<div className="flex items-center text-lg uppercase pt-8 mt-12 border-t-2 border-black opacity-10">
-			{themes.map(theme => {
+		<div className="flex items-center text-lg uppercase pt-8 mt-12">
+			{themes.map((theme) => {
 				const style = {
 					background: `linear-gradient(to left, ${theme.light} 50%, ${theme.dark} 50%)`,
 				};
@@ -63,12 +63,18 @@ export const ColorPicker = () => {
 							name="color"
 						/>
 						<label style={style} htmlFor={theme.name}></label>
-
 					</>
 				);
 			})}
 			<button onClick={toggleMode}>
-				<svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="25" height="22"><path d="M24.8 6.842H3.362l3.485 4.216-.908.967L.2 6.051 5.94.018l.908.966-3.485 4.218H24.8v1.64zm0 9.106l-5.74 6.034-.909-.967 3.486-4.217H.2v-1.64h21.436l-3.486-4.217.908-.966 5.74 5.973z" font-size="29.285" font-weight="400" font-family="Arial"/></svg>
+				<svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="25" height="22">
+					<path
+						d="M24.8 6.842H3.362l3.485 4.216-.908.967L.2 6.051 5.94.018l.908.966-3.485 4.218H24.8v1.64zm0 9.106l-5.74 6.034-.909-.967 3.486-4.217H.2v-1.64h21.436l-3.486-4.217.908-.966 5.74 5.973z"
+						font-size="29.285"
+						font-weight="400"
+						font-family="Arial"
+					/>
+				</svg>
 			</button>
 		</div>
 	);
